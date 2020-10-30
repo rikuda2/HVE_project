f9bd72f2fa6e170ea47c17270ed558c5821105b5
Berin <berin.balje@gmail.com>
2020-10-30 08:43:46 +0100

    Written steps/tasks that needed to be done

--- NumericalSolution.m
+++ NumericalSolution.m
@@ -1,1 +1,10 @@
 %Here we will attempt to solve numberically the solution, using ode 
+
+%Step 1: Define parameter ranges
+%Step 2: Create Diff eq. Solver
+%Step 3: Loop throuth the parameters and obtain solution for each parameter
+%set
+%Step 4: Check the solution with the satandards
+%Step 5: If the solution passes the starndard save it if not discard
+%Step 6: Tabulate the solution that passed the standards 
+
